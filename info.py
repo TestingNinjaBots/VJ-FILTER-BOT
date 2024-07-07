7d90a63cdfd3ded6fa95da03597635196c600c22
TestingNinjaBots <bharat.storage001@gmail.com>
2024-07-07 11:12:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -138,7 +138,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "unpleasant-fay-technicalhubtests1-7c5e178b.koyeb.app/")
+URL = environ.get("URL", "https://unpleasant-fay-technicalhubtests1-7c5e178b.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
